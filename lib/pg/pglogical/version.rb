613eacf40c2db0650dec28cd1913d9fc5f1146fd
Nick Carboni <ncarboni@redhat.com>
2016-12-19 13:30:45 -0500

    Initial commit as gem

---
+++ lib/pg/pglogical/version.rb
@@ -0,0 +1,5 @@
+module Pg
+  module Pglogical
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/pg/pglogical_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe Pg::Pglogical do
+  it "has a version number" do
+    expect(Pg::Pglogical::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
