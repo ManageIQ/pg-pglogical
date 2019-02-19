6b8a4f930a8cf1350cd0bf4ce944e095446d310f
Nick Carboni <ncarboni@redhat.com>
2019-02-19 14:29:54 -0500

    Update ci for postgres 10

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -4,10 +4,10 @@ sudo sh -c 'echo "deb http://packages.2ndquadrant.com/pglogical/apt/ $(lsb_relea
 
 wget --quiet -O - http://packages.2ndquadrant.com/pglogical/apt/AA7A6805.asc | sudo apt-key add -
 sudo apt-get -y update
-sudo apt-get -y install postgresql-9.5-pglogical
+sudo apt-get -y install postgresql-10-pglogical
 
-echo -e "wal_level = 'logical'\nmax_worker_processes = 10\nmax_replication_slots = 10\nmax_wal_senders = 10\nshared_preload_libraries = 'pglogical'" | sudo tee -a /etc/postgresql/9.5/main/postgresql.conf
-echo -e "local replication all trust" | sudo tee -a /etc/postgresql/9.5/main/pg_hba.conf
-sudo service postgresql restart 9.5
+echo -e "wal_level = 'logical'\nmax_worker_processes = 10\nmax_replication_slots = 10\nmax_wal_senders = 10\nshared_preload_libraries = 'pglogical'" | sudo tee -a /etc/postgresql/10/main/postgresql.conf
+echo -e "local replication all trust" | sudo tee -a /etc/postgresql/10/main/pg_hba.conf
+sudo service postgresql restart 10
 
 set +v
